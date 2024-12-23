165f5d4bf388a516c6c54df7c2476a50e94c46df
Omar <oa196145@gmail.com>
2024-12-23 19:54:12 +0200

    stashing

---
+++ Mini_InstaPay/Mini_InstaPay/TwoFactorAuthManager.cs
@@ -0,0 +1,37 @@
+namespace Mini_InstaPay
+{
+    public class TwoFactorAuthManager
+    {
+        public void Authenticate()
+        {
+            string otp = OtpGenerator.Instance.GenerateOtp();
+
+            Console.WriteLine($"Your OTP is: {otp}");
+
+            Console.WriteLine("Enter the OTP you received:");
+            string userOtp = Console.ReadLine() ?? "";
+
+            if (userOtp == otp)
+            {
+                Console.WriteLine("OTP verified successfully! Authentication complete.");
+
+                    // EncryptionManager encryptionManager = new EncryptionManager();
+
+                    //  Console.WriteLine("Enter sensitive data to encrypt : ");
+                    // string sensitiveData = Console.ReadLine() ?? "";
+
+                    //   string encryptedData = encryptionManager.Encrypt(sensitiveData);
+                    //   Console.WriteLine($"Encrypted Data: {encryptedData}");   
+
+                    //   string decryptedData = encryptionManager.Decrypt(encryptedData);
+                    //   Console.WriteLine($"Decrypted Data: {decryptedData}");   
+
+    
+            }
+            else
+            {
+                Console.WriteLine("Invalid OTP. Authentication failed.");
+            }
+        }
+    }
+}
