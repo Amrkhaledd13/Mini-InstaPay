8c6d1fb9ccaea1434ac6b6185005486b6ce822c9
wantedali <134491027+wantedali@users.noreply.github.com>
2024-12-25 04:19:01 +0200

    op

--- GUI-mini_insta/obj/Debug/net8.0-windows/GUI-mini_insta.AssemblyInfo.cs
+++ GUI-mini_insta/obj/Debug/net8.0-windows/GUI-mini_insta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI-mini_insta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2215734772da35a6995afc2f625cb25c58423d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d542186ea5b7ea5984130d2d48a7d854ff6683ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI-mini_insta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI-mini_insta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
